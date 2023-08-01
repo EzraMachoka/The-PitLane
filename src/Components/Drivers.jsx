6d21d52e3167294ac6918af077839a29f1039fde
Ezra Machoka <ezra.machoka@student.moringaschool.com>
2023-08-01 17:29:34 +0300

    Mapping and displaying the data

--- src/Components/Drivers.jsx
+++ src/Components/Drivers.jsx
@@ -1,14 +1,36 @@
-import React, {useEffect} from 'react'
+import React, { useEffect, useState } from "react";
 
 const Drivers = () => {
-    useEffect(() => {
-        fetch("http://ergast.com/api/f1/drivers.json")
-          .then((r) => r.json())
-          .then((data) => console.log(data.MRData));
-      }, []);
+    const [drivers, setdrivers] = useState([])
+  useEffect(() => {
+    fetch("http://ergast.com/api/f1/drivers.json")
+      .then((r) => r.json())
+      .then((data) => setdrivers(data.MRData.DriverTable.Drivers));
+  }, []);
   return (
-    <div>Drivers</div>
-  )
-}
+    <div className="grid grid-cols-4 gap-4">
+      {drivers.map((driver) => (
+        <div
+          className="card grid-item"
+          style={{ width: "18rem" }}
+          key={driver.driverId}
+        >
+          <img
+            src='https://e6.365dm.de/23/04/768x432/skysport_de-hamilton-mercedes_6126624.jpg?20230419183830'
+            className="card-img-top"
+            alt="Galactic Warrior"
+            style={{width:"200px"}}
+          />
+          <div className="card-body">
+            <h5 className="card-title">{driver.givenName}</h5>
+            <p className="card-text">Armor: {driver.familyName}</p>
+            <p className="card-text">Damage: {driver.dateOfBirth}</p>
+            <p className="cardtext">Health: {driver.nationality}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default Drivers
+export default Drivers;
