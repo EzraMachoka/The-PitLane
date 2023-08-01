2e9699b2fb9549d703f33198235bc126b3c6b109
Ezra Machoka <ezra.machoka@student.moringaschool.com>
2023-08-01 14:05:09 +0300

    Creating Routes in the Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-
-import Drivers from './Components/Drivers'
-import About from './Components/About'
-import Home from './Components/Home'
-import Navbar from './Components/Navbar'
+import Drivers from "./Components/Drivers";
+import About from "./Components/About";
+import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  
-
   return (
-    <>
-    <Navbar/>
-     <Drivers/>
-     <About/>
-     <Home/>
-    </>
-  )
+    // Helps to navigate to whatever has been clicked 
+    <BrowserRouter> 
+      <Navbar />
+      <Routes>
+      {/* creating a route to drivers using BrowserRouter */}
+        <Route path="/drivers" element={<Drivers />}/>        
+        <Route path="/about" element={<About />}/>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+        <Link to='/'>
+            Home
+        </Link>
+        <Link to='/drivers'>
+            Drivers
+        </Link>
+        <Link to='/about'>
+            About
+        </Link>
+    </div>
   )
 }
 
