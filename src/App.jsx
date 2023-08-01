3f0ca8c4d5f7ca77ff27b6e7de31ae46190ee7ba
Ezra Machoka <ezra.machoka@student.moringaschool.com>
2023-08-01 13:14:14 +0300

    importing all the components into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import Drivers from './Components/Drivers'
+import About from './Components/About'
+import Home from './Components/Home'
 
 function App() {
   const [count, setCount] = useState(0)
